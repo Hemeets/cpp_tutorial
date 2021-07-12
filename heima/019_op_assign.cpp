7200a4d14b6d761c54849af135f508e50c334a0b
qidongxu <qidongxu@tencent.com>
2021-07-12 20:25:19 +0800

    0712

---
+++ heima/019_op_assign.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+    cout << "hello world!" << endl;
+
+    // 赋值运算符
+    int a = 10;
+    cout << "a = " << a << endl;
+
+    a += 2;
+    cout << "a = " << a << endl;
+    
+    return 0;
+}
---
+++ heima/020_op_compare.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+    cout << "hello world!" << endl;
+
+    // ==, !=, <, >, <=, >=
+    int a = 10;
+    int b = 10;
+    cout << (a == b) << endl;  // 注意括号
+
+    
+    return 0;
+}
