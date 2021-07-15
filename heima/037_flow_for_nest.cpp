9946dd65103a7ee89445b8a2c8f9061a80f7d0ca
qidongxu <qidongxu@tencent.com>
2021-07-15 20:47:29 +0800

    0715

---
+++ heima/037_flow_for_nest.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+
+int main() {
+    cout << "hello world!" << endl;
+
+    for (int j = 0; j < 10; j++)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+
+    
+    return 0;
+}
